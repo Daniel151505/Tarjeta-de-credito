b788e73f97afdb06b930d0ada67e0201f13bf921
Daniel151505 <daniel150520011@gmail.com>
2020-10-19 22:47:38 -0500

    Configuración de el DbContext

--- BackEnd/BackEnd/Startup.cs
+++ BackEnd/BackEnd/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
 namespace BackEnd
 {
@@ -25,6 +26,7 @@ namespace BackEnd
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
             services.AddControllers();
         }
 
@@ -45,6 +47,7 @@ namespace BackEnd
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+             
             });
         }
     }
